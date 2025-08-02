a3dc7a48512a0fbe495eaa2831a03b9ad8623e44
BhavanaBgowda13 <bhavana.b@s.amity.edu>
2025-08-02 23:35:23 +0530

    p3

--- pr.c
+++ pr.c
@@ -4,4 +4,8 @@ int main() {
     int num;
 printf("Enter a positive integer: ");
     scanf("%d", &num);
+if (num < 2) {
+        printf("%d is not a prime number.\n", num);
+    } else {
+        int i, isPrime = 1;
 
