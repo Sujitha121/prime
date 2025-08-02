664910e11c6b9f63d3ac94288136cf7615c61779
BhavanaBgowda13 <bhavana.b@s.amity.edu>
2025-08-02 23:36:20 +0530

    p4

--- pr.c
+++ pr.c
@@ -8,4 +8,10 @@ if (num < 2) {
         printf("%d is not a prime number.\n", num);
     } else {
         int i, isPrime = 1;
+for (i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
 
